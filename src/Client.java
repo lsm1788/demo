c778d4053b04f03cdd5d9974eca4d32eaaddb249
seungmokLee <lsm17888@naver.com>
2022-01-20 13:36:37 +0900

    sever

--- src/Client.java
+++ src/Client.java
@@ -1,9 +1,42 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
 public class Client {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	final static String SERVER_IP = "203.228.62.31"; // "final"상수는 대문자 사용,단어와 단어사이에 '_' 사용
+	final static int SERVER_PORT = 1225;
+	final static String MESSAGE_TO_SERVER = "Hi, Sever!";
 
+	public static void main(String[] args) {
+		Socket socket = null;
+		try {
+			socket = new Socket(SERVER_IP, SERVER_PORT);
+			System.out.println("socket 연결");
+			
+			InputStream is = socket.getInputStream();
+			OutputStream os = socket.getOutputStream();
+			
+			os.write(MESSAGE_TO_SERVER.getBytes());
+			os.flush();
+			
+			byte[] data = new byte[16];
+			int n = is.read(data);
+			final String messageFromClient = new String(data,0,n);
+			System.out.println(messageFromClient);
+			
+		
+			
+			is.close();
+			os.close();
+			
+			socket.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 }
---
+++ src/Sever.java
@@ -0,0 +1,46 @@
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Sever extends Thread {
+	
+	final static int SERVER_PORT = 1225;
+	final static String MESSAGE_TO_SERVER = "Hello, Client!";
+	
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+			try {
+				serverSocket = new ServerSocket(SERVER_PORT);
+			
+				while(true) {
+					System.out.println("socket 연결 대기");
+					Socket socket = serverSocket.accept();
+					System.out.println("host"+socket.getInetAddress()+"통신 연결성공");
+					
+					InputStream is = socket.getInputStream();
+					OutputStream os = socket.getOutputStream();
+					
+					byte[] data = new byte[16];
+					int n = is.read(data);
+					final String messageFromClient = new String(data,0,n);
+					System.out.println(messageFromClient);
+					
+					os.write(MESSAGE_TO_SERVER.getBytes());
+					os.flush();
+					
+					is.close();
+					os.close();
+					
+					
+					socket.close();
+				}
+				
+			} catch (Exception e) {
+				
+				e.printStackTrace();
+			}
+			
+	}
+
+}
